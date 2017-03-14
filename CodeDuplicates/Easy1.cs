1e8fc8d767a6a0c268aa6ff47310aa972a28e270
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-15 03:11:25 +1100

    Initial commit.

---
+++ CodeDuplicates/Easy1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeDuplicates
+{
+    public class Easy1
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
