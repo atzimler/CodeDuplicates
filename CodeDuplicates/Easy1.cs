f185f05e5c264f61c807b34c5e4c9216f53c8abd
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-15 04:38:24 +1100

    Let's see if this will be detected by dupfinder.

--- CodeDuplicates/Easy1.cs
+++ CodeDuplicates/Easy1.cs
@@ -2,6 +2,9 @@
 
 namespace CodeDuplicates
 {
+    /// <summary>
+    /// It seems that this was a so easy duplicate that the cost was marked 0 by R#CLT. Which indicates no problem.
+    /// </summary>
     public class Easy1
     {
         public void SayHello()
---
+++ CodeDuplicates/TestCodeDuplication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeDuplicates
+{
+    [TestFixture]
+    public class TestCodeDuplication
+    {
+        [Test]
+        public void DivisionByZeroThrowsAnException()
+        {
+            var number = 42;
+            var divider = 0;
+            int result;
+
+            Assert.Throws<DivideByZeroException>(() => result = number / divider);
+        }
+
+        [Test]
+        public void YouShouldNotDivideANumberByZero()
+        {
+            var number = 13;
+            var divider = 0;
+            int result;
+
+            Assert.Throws<DivideByZeroException>(() => result = number / divider);
+        }
+    }
+}
