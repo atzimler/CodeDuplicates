dc9c30d5003ae73e444e4b7063b9d3e8e2db8efc
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-01 08:59:39 +1100

    Ccm25

--- CCMTest/CcmMethods.cs
+++ CCMTest/CcmMethods.cs
@@ -243,6 +243,90 @@ namespace CCMTest
             }
         }
 
+
+        public void Ccm25()
+        {
+            var random = new Random();
+            // ReSharper disable once SwitchStatementMissingSomeCases => R# Please ignore this, would never write this code if not testing bad ones, thanks.
+            switch (random.Next())
+            {
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+
+                case 11:
+                    break;
+
+                case 12:
+                    break;
+
+                case 13:
+                    break;
+
+                case 14:
+                    break;
+
+                case 15:
+                    break;
+
+                case 16:
+                    break;
+
+                case 17:
+                    break;
+
+                case 18:
+                    break;
+
+                case 19:
+                    break;
+
+                case 20: // OK, so CCM 20 is only hitting normal priority in Jenkins plug-in. I have a higher standard :D (do not write something more complex than 5).
+                    break;
+
+
+                case 21:
+                    break;
+
+                case 22:
+                    break;
+
+                case 23:
+                    break;
+
+                case 24:
+                    break;
+
+                case 25:
+                    break;
+
+            }
+        }
+
         public void Ccm30()
         {
             var random = new Random();
