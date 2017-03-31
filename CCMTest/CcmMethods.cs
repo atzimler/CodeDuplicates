15f5512b71b47c0bda85db45e3f3811b639112f3
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-01 09:06:37 +1100

    Ok, where is the limit between low and normal CCM warnings?

--- CCMTest/CcmMethods.cs
+++ CCMTest/CcmMethods.cs
@@ -243,6 +243,42 @@ namespace CCMTest
             }
         }
 
+        public void Ccm10()
+        {
+            var random = new Random();
+            // ReSharper disable once SwitchStatementMissingSomeCases => R# Please ignore this, would never write this code if not testing bad ones, thanks.
+            switch (random.Next())
+            {
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+            }
+        }
+
+
         public void Ccm20()
         {
             var random = new Random();
