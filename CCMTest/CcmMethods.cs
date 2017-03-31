cd220ee84bf38f290512a2b595d964191d6c631e
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-01 09:08:08 +1100

    Ccm15.

--- CCMTest/CcmMethods.cs
+++ CCMTest/CcmMethods.cs
@@ -278,6 +278,56 @@ namespace CCMTest
             }
         }
 
+        public void Ccm15()
+        {
+            var random = new Random();
+            // ReSharper disable once SwitchStatementMissingSomeCases => R# Please ignore this, would never write this code if not testing bad ones, thanks.
+            switch (random.Next())
+            {
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+
+                case 11:
+                    break;
+
+                case 12:
+                    break;
+
+                case 13:
+                    break;
+
+                case 14:
+                    break;
+
+                case 15:
+                    break;
+            }
+        }
 
         public void Ccm20()
         {
