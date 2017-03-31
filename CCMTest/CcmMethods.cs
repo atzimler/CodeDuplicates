127f78565befb6d679d526cdbade18d56a2ee824
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-01 08:52:36 +1100

    Let's see what will be the complexity for the Jenkins CCM plugin for a CCM 50.

--- CCMTest/CcmMethods.cs
+++ CCMTest/CcmMethods.cs
@@ -145,7 +145,100 @@ namespace CCMTest
                 case 19:
                     break;
 
-                case 20:
+                case 20: // OK, so CCM 20 is only hitting normal priority in Jenkins plug-in. I have a higher standard :D (do not write something more complex than 5).
+                    break;
+
+
+                case 21:
+                    break;
+
+                case 22:
+                    break;
+
+                case 23:
+                    break;
+
+                case 24:
+                    break;
+
+                case 25:
+                    break;
+
+                case 26:
+                    break;
+
+                case 27:
+                    break;
+
+                case 28:
+                    break;
+
+                case 29:
+                    break;
+
+                case 30:
+                    break;
+
+
+                case 31:
+                    break;
+
+                case 32:
+                    break;
+
+                case 33:
+                    break;
+
+                case 34:
+                    break;
+
+                case 35:
+                    break;
+
+                case 36:
+                    break;
+
+                case 37:
+                    break;
+
+                case 38:
+                    break;
+
+                case 39:
+                    break;
+
+                case 40:
+                    break;
+
+
+                case 41:
+                    break;
+
+                case 42:
+                    break;
+
+                case 43:
+                    break;
+
+                case 44:
+                    break;
+
+                case 45:
+                    break;
+
+                case 46:
+                    break;
+
+                case 47:
+                    break;
+
+                case 48:
+                    break;
+
+                case 49:
+                    break;
+
+                case 50:
                     break;
             }
         }
