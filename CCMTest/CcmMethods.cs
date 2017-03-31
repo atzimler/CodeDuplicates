0e01d5ddf6aaee6eeccb4588c4374feb55f0cee3
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-01 08:47:37 +1100

    Adding CCM 20 method to test it.

--- CCMTest/CcmMethods.cs
+++ CCMTest/CcmMethods.cs
@@ -83,5 +83,71 @@ namespace CCMTest
                     break;
             }
         }
+
+        public void LetsConstructAReallyComplexMethodBecauseIAmInterestedInThePriorityThresholdsOfJenkinsCcmPlugin()
+        {
+            var random = new Random();
+            // ReSharper disable once SwitchStatementMissingSomeCases => R# Please ignore this, would never write this code if not testing bad ones, thanks.
+            switch (random.Next())
+            {
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+
+                case 11:
+                    break;
+
+                case 12:
+                    break;
+
+                case 13:
+                    break;
+
+                case 14:
+                    break;
+
+                case 15:
+                    break;
+
+                case 16:
+                    break;
+
+                case 17:
+                    break;
+
+                case 18:
+                    break;
+
+                case 19:
+                    break;
+
+                case 20:
+                    break;
+            }
+        }
     }
 }
