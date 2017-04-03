1b8745ad9d50f91570aea8521de1a47f966c53c1
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-03 23:12:23 +1000

    Let's do VB complexity.

---
+++ ClassLibrary1/LetsDoVbComplexity.vb
@@ -0,0 +1,70 @@
+﻿Public Class LetsDoVbComplexity
+    Public Sub CcmX()
+        Dim random = New Random
+        Select random.Next Mod 50
+            Case 0:
+                    Console.WriteLine("X")
+            Case 1:
+                    Console.WriteLine("X")
+            Case 2:
+                    Console.WriteLine("X")
+            Case 3:
+                    Console.WriteLine("X")
+            Case 4:
+                    Console.WriteLine("X")
+            Case 5:
+                    Console.WriteLine("X")
+            Case 6:
+                    Console.WriteLine("X")
+            Case 7:
+                    Console.WriteLine("X")
+            Case 8:
+                    Console.WriteLine("X")
+            Case 9:
+                    Console.WriteLine("X")
+            Case 10:
+                    Console.WriteLine("X")
+            Case 11:
+                    Console.WriteLine("X")
+            Case 12:
+                    Console.WriteLine("X")
+            Case 13:
+                    Console.WriteLine("X")
+            Case 14:
+                    Console.WriteLine("X")
+            Case 15:
+                    Console.WriteLine("X")
+            Case 16:
+                    Console.WriteLine("X")
+            Case 17:
+                    Console.WriteLine("X")
+            Case 18:
+                    Console.WriteLine("X")
+            Case 19:
+                    Console.WriteLine("X")
+            Case 20:
+                    Console.WriteLine("X")
+            Case 21:
+                    Console.WriteLine("X")
+            Case 22:
+                    Console.WriteLine("X")
+            Case 23:
+                    Console.WriteLine("X")
+            Case 24:
+                    Console.WriteLine("X")
+            Case 25:
+                    Console.WriteLine("X")
+            Case 26:
+                    Console.WriteLine("X")
+            Case 27:
+                    Console.WriteLine("X")
+            Case 28:
+                    Console.WriteLine("X")
+            Case 29:
+                    Console.WriteLine("X")
+            Case 30:
+                    Console.WriteLine("X")
+        End Select
+
+    End Sub
+End Class
